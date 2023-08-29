d781355343bb6fa903fbf342796d09540bafc8fd
João lucas <jlvatejo123@gmail.com>
2023-08-29 13:23:36 -0300

    new project

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+
+const limit = 10
+const offset = 0
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(jsonBody){
+        console.log(jsonBody)
+    })
+    .catch(function(error){
+        console.error(error)
+    })
+    .finally(function(){
+        console.log('Requisição concluida!')
+    })
